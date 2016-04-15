246598e81ba829aeb8d91153c39cabb495969ea4
Jean-Philippe Braun <jean-philippe.braun@cloudwatt.com>
2016-04-15 15:18:07 +0200

    Avoid destroying servicechains on schema init
    Change-Id: Ibc23f6f84c588e8fddd2d8c02ea7adeacf754742
    Closes-Bug: #1570857
    (cherry picked from commit 8f64ab519cdb73b52cb1cef18eac0ec11b381419)

--- src/config/schema-transformer/to_bgp.py
+++ src/config/schema-transformer/to_bgp.py
@@ -3390,6 +3390,7 @@ class SchemaTransformer(object):
 
     def process_poll_result(self, poll_result_str):
         something_done = False
+        initial_search = False
         result_list = parse_poll_result(poll_result_str)
         self.current_network_set = set()
 
@@ -3397,9 +3398,9 @@ class SchemaTransformer(object):
         for (result_type, idents, metas) in result_list:
             if result_type != 'searchResult' and not self.ifmap_search_done:
                 self.ifmap_search_done = True
-                self.process_stale_objects()
-                self.current_network_set = VirtualNetworkST.keys()
+                self.current_network_set = set(VirtualNetworkST.keys())
                 something_done = True
+                initial_search = True
             for meta in metas:
                 meta_name = re.sub('{.*}', '', meta.tag)
                 if result_type == 'deleteResult':
@@ -3425,6 +3426,8 @@ class SchemaTransformer(object):
             return
         if self.ifmap_search_done:
             self.process_networks()
+        if initial_search:
+            self.process_stale_objects()
     # end process_poll_results
 
     def process_networks(self):
@@ -3814,9 +3817,9 @@ class SchemaTransformer(object):
 def set_ifmap_search_done(transformer):
     gevent.sleep(60)
     transformer.ifmap_search_done = True
-    transformer.process_stale_objects()
     transformer.current_network_set = list(VirtualNetworkST.keys())
     transformer.process_networks()
+    transformer.process_stale_objects()
 # end set_ifmap_search_done
 
 def launch_arc(transformer, ssrc_mapc):
